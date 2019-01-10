80218e211280dc06dbd0875e6c712f5e81ca87ae
GrantBerland <grant.berland.gb@gmail.com>
2019-01-10 13:35:20 -0700

    first commit

---
+++ src/DetectorConstruction.cc
@@ -0,0 +1,171 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// $Id: DetectorConstruction.cc 94307 2015-11-11 13:42:46Z gcosmo $
+//
+/// \file DetectorConstruction.cc
+/// \brief Implementation of the DetectorConstruction class
+
+#include "DetectorConstruction.hh"
+
+#include "G4RunManager.hh"
+#include "G4NistManager.hh"
+#include "G4Box.hh"
+#include "G4Tubs.hh"
+#include "G4Cons.hh"
+#include "G4Orb.hh"
+#include "G4Sphere.hh"
+#include "G4GenericPolycone.hh"
+#include "G4Trd.hh"
+#include "G4LogicalVolume.hh"
+#include "G4PVPlacement.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4IntersectionSolid.hh"
+#include "G4RotationMatrix.hh"
+
+#include <fstream>
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+DetectorConstruction::DetectorConstruction()
+: G4VUserDetectorConstruction(),
+  fScoringVolume(0)
+{ }
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+DetectorConstruction::~DetectorConstruction()
+{ }
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G4VPhysicalVolume* DetectorConstruction::Construct()
+{
+  // Get nist material manager
+  G4NistManager* nist = G4NistManager::Instance();
+
+  // Envelope parameters
+  //
+  G4double env_sizeXY = 20*cm, env_sizeZ = 30*cm;
+
+    // Material: Vacuum
+    //TODO: check pressures, environment for Van Allen belt altitudes
+  G4Material* vacuum_material = new G4Material("Vacuum",
+              1.0 , 1.01*g/mole, 1.0E-25*g/cm3,
+              kStateGas, 2.73*kelvin, 3.0E-18*pascal );
+
+  // Option to switch on/off checking of volumes overlaps
+  //
+  G4bool checkOverlaps    = true;
+  G4bool validateSchema = false;
+
+  fParser.Read("../GDML_files/shielding_out.gdml", validateSchema);
+
+  //
+  // World
+  //
+  G4double world_sizeXY = 1.2*env_sizeXY;
+  G4double world_sizeZ  = 1.2*env_sizeZ;
+
+  G4Box* solidWorld =
+    new G4Box("World",                       //its name
+       0.5*world_sizeXY, 0.5*world_sizeXY, 0.5*world_sizeZ);     //its size
+
+  G4LogicalVolume* logicWorld =
+    new G4LogicalVolume(solidWorld,          //its solid
+                        vacuum_material,           //its material
+                        "World");            //its name
+
+  G4VPhysicalVolume* physWorld =
+    new G4PVPlacement(0,                     //no rotation
+                      G4ThreeVector(),       //at (0,0,0)
+                      logicWorld,            //its logical volume
+                      "World",               //its name
+                      0,                     //its mother  volume
+                      false,                 //no boolean operation
+                      0,                     //copy number
+                      checkOverlaps);        //overlaps checking
+
+  //
+  // Envelope
+  //
+  G4Box* solidEnv =
+    new G4Box("Envelope",                    //its name
+        0.5*env_sizeXY, 0.5*env_sizeXY, 0.5*env_sizeZ); //its size
+
+  G4LogicalVolume* logicEnv =
+    new G4LogicalVolume(solidEnv,            //its solid
+                        vacuum_material,             //its material
+                        "Envelope");         //its name
+
+  new G4PVPlacement(0,                       //no rotation
+                    G4ThreeVector(),         //at (0,0,0)
+                    logicEnv,                //its logical volume
+                    "Envelope",              //its name
+                    logicWorld,              //its mother  volume
+                    false,                   //no boolean operation
+                    0,                       //copy number
+                    checkOverlaps);          //overlaps checking
+
+
+
+  G4ThreeVector ele_pos  = G4ThreeVector(2.5*cm,3.5*cm,1.0*cm);
+  G4VSolid* electronics_box = new G4Box("Electronics",
+                                    1.5*cm, 1.5*cm, 0.1*cm);
+
+
+  G4LogicalVolume* electronics_LV =
+  new G4LogicalVolume(electronics_box,                     //its solid
+                      nist->FindOrBuildMaterial("G4_Si"),  //its material
+                      "Electronics");                        //its name
+
+  new G4PVPlacement(0,                     //no rotation
+                  ele_pos,                 //at position
+                  electronics_LV,          //its logical volume
+                  "Electronics",           //its name
+                  logicEnv,                //its mother  volume
+                  false,                   //no boolean operation
+                  0,                       //copy number
+                  checkOverlaps);          //overlaps checking
+
+
+  G4LogicalVolume* shielding_LV = fParser.GetVolume("shielding2_Tungsten.STL");
+
+  new G4PVPlacement(0,
+                    G4ThreeVector(),
+                    shielding_LV,
+                    "shielding2_Tungsten.STL",
+                    logicEnv,
+                    false,
+                    0,
+                    checkOverlaps);
+
+  fScoringVolume = electronics_LV;
+
+  // always return the physical World
+  return physWorld;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
